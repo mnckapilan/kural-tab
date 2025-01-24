b10e8836720f98ea26d47036b796c8e15a2d1e2a
Kapilan M <mnckapilan@gmail.com>
2025-01-24 20:59:20 +0000

    Change folder structure

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: "chrome://newtab" }, (newTab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: newTab.id },
-      files: ["index.html"],
-    });
-  });
-});
