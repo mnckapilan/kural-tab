14b641314b6fd3ce57ad9d01ca0a88cb73ba312f
Kapilan M <mnckapilan@gmail.com>
2025-12-29 22:14:00 +0000

    Adds dev build indicator

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,13 @@
 const { merge } = require("webpack-merge");
+const webpack = require("webpack");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(false),
+    }),
+  ],
 });
